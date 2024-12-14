12b2d84843828c7821e4fa5b84c8875a3473833b
A-freire <bexas94@gmail.com>
2024-12-14 22:45:16 +0100

    init project

---
+++ Watcher/Watcher/WatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatcherApp.swift
+//  Watcher
+//
+//  Created by Adrien Freire on 14/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct WatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
