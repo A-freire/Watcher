04a22cb02cc7760b6e97ab98c8baf806cda1ea7c
A-freire <bexas94@gmail.com>
2024-12-26 13:04:59 +0100

    extension Color Hex

---
+++ Watcher/Watcher/Extension/Color.swift
@@ -0,0 +1,36 @@
+//
+//  Color.swift
+//  Watcher
+//
+//  Created by Adrien Freire on 25/12/2024.
+//
+
+import SwiftUI
+
+extension Color {
+    static func colorFromHex(_ hex: String) -> Color {
+        var hexSanitized = hex.trimmingCharacters(in: .whitespacesAndNewlines)
+        hexSanitized = hexSanitized.hasPrefix("#") ? String(hexSanitized.dropFirst()) : hexSanitized
+        
+        let length = hexSanitized.count
+        guard length == 6 || length == 8 else { return .black }
+        
+        var rgb: UInt64 = 0
+        Scanner(string: hexSanitized).scanHexInt64(&rgb)
+        
+        let r, g, b, a: Double
+        if length == 6 {
+            r = Double((rgb & 0xFF0000) >> 16) / 255.0
+            g = Double((rgb & 0x00FF00) >> 8) / 255.0
+            b = Double(rgb & 0x0000FF) / 255.0
+            a = 1.0
+        } else {
+            r = Double((rgb & 0xFF000000) >> 24) / 255.0
+            g = Double((rgb & 0x00FF0000) >> 16) / 255.0
+            b = Double((rgb & 0x0000FF00) >> 8) / 255.0
+            a = Double(rgb & 0x000000FF) / 255.0
+        }
+        
+        return Color(red: r, green: g, blue: b, opacity: a)
+    }
+}
